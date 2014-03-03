6d79ea32e5c11e0c2581e98fc7a3453e39788fb9
Michael Ryan <falter@cpan.org>
2014-03-03 19:18:15 +0000

    Import of FALTER/Cikl-0.03 from CPAN.
    gitpan-cpan-distribution: Cikl
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         FALTER/Cikl-0.03.tar.gz
    gitpan-cpan-author:       FALTER
    gitpan-cpan-maturity:     released

---
+++ lib/Cikl/DataTypes/LowercaseUUID.pm
@@ -0,0 +1,14 @@
+package Cikl::DataTypes::LowercaseUUID;
+use strict;
+use warnings;
+use namespace::autoclean;
+use Mouse::Util::TypeConstraints;
+use Cikl qw(is_uuid);
+
+subtype "Cikl::DataTypes::LowercaseUUID", 
+  as 'Str',
+  where { is_uuid($_) },
+  message { "Not a UUID with all lower-case characters: " . $_ };
+
+1;
+
